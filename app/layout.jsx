5ac65244e7352c2fbbeba809cf7cd4ff4baa3f15
Van Huy <1712056@student.hcmus.edu.vn>
2023-05-29 16:59:49 +0700

    implement signIn with google using next-auth

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,18 +1,25 @@
 import "@styles/global.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metaData = {
   title: "Promptopia",
   discription: "Discover & share AI Prompts",
 };
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-      <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
-        <main className="app">{children}</main>
-      </body>
+      <Provider>
+        <body>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </body>
+      </Provider>
     </html>
   );
 };
