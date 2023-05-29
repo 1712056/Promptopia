61d45fb04857afe738e7bbeca0d1d9d7abb48956
Van Huy <1712056@student.hcmus.edu.vn>
2023-05-29 10:47:12 +0700

    init project

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import "@styles/global.css";
+
+export const metaData = {
+  title: "Promptopia",
+  discription: "Discover & share AI Prompts",
+};
+const RootLayout = ({children}) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
